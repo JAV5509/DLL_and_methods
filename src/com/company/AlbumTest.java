b9ba7982e2da822f966e1fc6a09b2044e8a37a3f
julianvazquez <jav5509@psu.edu>
2022-11-08 21:55:45 -0500

    Creating the Album constructor and empty constructor along with the test class

---
+++ src/com/company/AlbumTest.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlbumTest {
+
+    @Test
+    void testAlbum(){
+
+
+        ArrayList<String> artistNames = new ArrayList<>();
+
+        artistNames.add("Artist 1");
+        artistNames.add("Artist 2");
+        artistNames.add("Artist 3");
+
+        Album tester_1 = new Album(2324,artistNames, "The Motions", 8);
+
+        Album tester_2 = new Album();
+
+        //testing the constructor
+
+        System.out.println(tester_1.toString());
+
+        System.out.println(tester_2.toString());
+
+
+    }
+
+}
